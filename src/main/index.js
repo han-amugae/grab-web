96ef47b9e9e6d0766a6212462d8748e0e221dfa1
han_amugae <amugae0125@gmail.com>
2022-06-11 19:15:04 +0900

    그랩마켓 웹

--- src/main/index.js
+++ src/main/index.js
@@ -5,9 +5,7 @@ import { Link } from "react-router-dom";
 function MainPage() {
   const [products, setProducts] = React.useState([]);
   React.useEffect(function () {
-    Axios.get(
-      "https://7743da48-23b3-43cf-b5fb-ec9e3053146f.mock.pstmn.io/products"
-    )
+    Axios.get("http://localhost:8080/products")
       .then(function (result) {
         const products = result.data.products;
         setProducts(products);
@@ -20,7 +18,7 @@ function MainPage() {
   return (
     <div>
       <div id="banner">
-        <img src="images/banners/banner1.png" />
+        <img src="/images/banners/banner1.png" />
       </div>
       <h1>판매되는 상품들</h1>
       <div id="product-list">
@@ -41,8 +39,8 @@ function MainPage() {
                   <div className="product-seller">
                     <img
                       className="product-avatar"
-                      alt="images/icons/avatar.pmg"
-                      src="images/icons/avatar.png"
+                      alt="/images/icons/avatar.pmg"
+                      src="/images/icons/avatar.png"
                     />
                     <span>{product.seller}</span>
                   </div>
